e82e6cf14f4fe162ee6cc8f32434727530714a20
Giedre <giedrotie@gmail.com>
2021-03-09 13:55:01 +0200

    js intro

---
+++ JS/00-notes.js
@@ -0,0 +1,6 @@
+// kintamieji - jo atmintyje yra laikoma informacija
+// salygis sakiniai - issakoti logika pagal ka daryti kai daugiau/maziau, tiesa/netiesa
+// ciklai - kai turime sarasa su informacija kuri yra vienodos strukturos (pvz su pazymiais - vidurkis/ didziausias/ maziausias pazymis)
+// funkcijos - aprasome logika, kuria norime perpanaudoti N kartu, tik su galimai skirtingais parametrais
+
+// event'ai (event listeners) - skirta stebeti ivykius (click, scroll, keypress ir pan) i kuriuos reikia sureaguoti iskvieciant norima funkcija
