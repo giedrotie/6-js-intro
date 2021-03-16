9b31254b776f86b2e2a299d4b1a0b03fa073a7cc
Giedre <giedrotie@gmail.com>
2021-03-16 23:12:00 +0200

    empty function

---
+++ JS/13-empty.js
@@ -0,0 +1,10 @@
+function tusciaFunkcija(){ // (tuscia) - nepriima jokiu kintamuju 
+                           // {tuscia} - neatlieka jokios vidines logikos
+return false;              // grazina boolean tipo reiksme 'false'
+}
+console.log(tusciaFunkcija());
+
+// visu funkciju pareiga yra gavus pradines reiksmes - parametrus
+// ir perleidus ja per viduje aprasyta logika,
+// grazinti rezultata t.y. return 'kazkas' uzdaviniui - false 
+// automatiskai raso return undefined
