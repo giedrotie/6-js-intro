5ed74bb518e590c6d0add84e0a1aca7856cfcf1f
Giedre <giedrotie@gmail.com>
2021-03-19 20:16:02 +0200

    keepHydrated commit

---
+++ codewars/keepHydrated.js
@@ -0,0 +1,19 @@
+// https://www.codewars.com/kata/582cb0224e56e068d800003c/javascript
+
+
+function litres(time) {
+    return 0;
+  }
+
+
+
+
+
+console.log(litres(2), '->', 'should return 1 litre');
+console.log(litres(1.4), '->',  'should return 0 litres');
+console.log(litres(12.3), '->', 'should return 6 litres');
+console.log(litres(0.82), '->', 'should return 0 litres');
+console.log(litres(11.8), '->', 'should return 5 litres');
+console.log(litres(1787), '->', 'should return 893 litres');
+console.log(litres(0), '->', 'should return 0 litres');
+ 
