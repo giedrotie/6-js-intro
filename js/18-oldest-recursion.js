160994946d518309cabbe62866dbe1e190524312
Giedre <giedrotie@gmail.com>
2021-03-22 10:31:15 +0200

    oldest

---
+++ js/18-oldest-recursion.js
@@ -0,0 +1,14 @@
+// - rekursija? rasti  didziausia skaiciu betkokio gilumo sarase
+//- kas vyriausias gimineje?
+//- kas turi daugiausiai vaiku gimineje?
+
+//esme yra eiti per visus sarasus ir is ju istraukti informacija
+
+//rekursija -> kaip eiti per medi?
+
+// GIMINES MEDYJE REIKIA RASTI KAS BUVO/YRA VYRIAUSIAS?
+
+function oldest(){
+    return 0;
+
+}
