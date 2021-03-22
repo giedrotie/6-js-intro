bee40f8e1ad25904e2a0da165fa09ec7d7c90991
Giedre <giedrotie@gmail.com>
2021-03-22 22:29:36 +0200

    sum all numbers in array

--- codewars/sumAllArrays-.js
+++ codewars/sumAllArrays-.js
@@ -1,5 +1,8 @@
 //https://www.codewars.com/kata/5594463eaf1701909c0000d4/train/javascript
 // susumuoti visus skaicius esancius sarasauose
+// pirma suma =0
+// antra einame per sarasus, sukuriame kintamaji saraso itaji nari
+
 
 function arraySum(arr) {
     let sum = 0;
@@ -9,14 +12,15 @@ function arraySum(arr) {
         const child = arr[i];
 
         if (Array.isArray(child)){
-           sum += arraySum(child)
+            sum += arraySum(child)
         } else if (Number.isFinite(child)){
-            sum+=child;
+            sum += child
         }
 
     }
+
     return sum;
-   
+
 }
 
 console.log(arraySum([1, 2]), '->', '3');
@@ -25,3 +29,18 @@ console.log(arraySum([1, 2, 3]),'->', '6');
 console.log('----------');
 console.log(arraySum([1, 2, [1, 2]]),'->', '6');
 console.log('----------');
+
+
+/* ************ kitas var **************** */
+
+function arraySum(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (Array.isArray(arr[i])) {
+        sum += arraySum(arr[i]);
+      } else if (typeof arr[i] === 'number') {
+        sum += arr[i];
+      }
+    }
+    return sum;
+  }
